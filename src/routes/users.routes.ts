5f2875d29db35b274b8d2307d00b5673d2524be9
Fabiano Palharini <fabianofalco@gmail.com>
2020-04-19 14:27:05 -0300

    adds avatar upload and static file serve

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
@@ -17,5 +23,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar_file_name'),
+  async (request, response) => {
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
