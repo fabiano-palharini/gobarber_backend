1554d85a16f2e704184532a4bfd8adf5b03534c2
Fabiano Palharini <fabianofalco@gmail.com>
2020-05-27 22:42:40 -0300

    covers all scenarios for CreateAppointmentService

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,15 +1,16 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 
-class AppointmentsRepository implements IAppointmentsRepository {
+class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[]=[];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-      const findAppointment = this.appointments.find(appointment => appointment.date === date);
+      const findAppointment = this.appointments.find(appointment => isEqual(appointment.date, date));
 
       return findAppointment;
   }
@@ -30,4 +31,4 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
 }
 
-export default AppointmentsRepository;
+export default FakeAppointmentsRepository;
