dcd741e6ea4797565e65378b40bd1e5434bebbff
Fabiano Palharini <fabianofalco@gmail.com>
2020-06-07 16:50:57 -0300

    providers check day availability

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -4,6 +4,7 @@ import { getRepository, Repository, Raw} from 'typeorm';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO copy';
 
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -19,6 +20,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
       where: { date },
     });
     return findAppointment;
+  }
+
+
+  public async findAllInDayFromProvider({ provider_id, day, month, year}: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = String(day).padStart(2,'0');
+    const parsedMonth = String(month).padStart(2,'0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(dateFieldName => `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`),
+      }
+    });
+
+    return appointments;
   }
 
 
